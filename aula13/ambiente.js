07284bd05eb8a6acd422b7e07fe671379c3b7a4d
Thiago <thicef@gmail.com>
2022-02-08 11:17:07 -0300

    repetições

---
+++ aula13/ambiente.js
@@ -0,0 +1,19 @@
+//console.log('Tudo bem?')
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+/*
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+*/
+
+for (var c = 1; c <= 6; c++){
+    console.log(`Passo ${c}`)
+}
